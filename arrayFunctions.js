9bfd4244171b0ba84367f1f5b7e52889515104e0
assadribhi <assadribhi@gmail.com>
2020-06-17 19:33:52 +0300

    Not Done with the last one though :((

--- arrayFunctions.js
+++ arrayFunctions.js
@@ -10,7 +10,15 @@
  */
 function getOdds(numbers) {
   // Your code here
+  let oddNumbers = [];
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] % 2 === 1) {
+      oddNumbers.push(numbers[i]);
+    }
+  }
+  return oddNumbers;
 }
+getOdds([1, 2, 3, 4, 5, 6, 7, 8, 9]);
 
 /**
  * getEvens(numbers):
@@ -24,8 +32,15 @@ function getOdds(numbers) {
  */
 function getEvens(numbers) {
   // Your code here
+  let evenNumbers = [];
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] % 2 === 0) {
+      evenNumbers.push(numbers[i]);
+    }
+  }
+  return evenNumbers;
 }
-
+getEvens([1, 2, 3, 4, 5, 6, 7, 8, 9]);
 /**
  * getDuplicateCount(x, numbers):
  * - receives a number `x`, and an array of numbers called `numbers`
@@ -37,7 +52,15 @@ function getEvens(numbers) {
  */
 function getDuplicateCount(x, numbers) {
   // Your code here
+  let duplicates = [];
+  for (let i = 0; i < numbers.length; i++) {
+    if (x === numbers[i]) {
+      duplicates.push(numbers[i]);
+    }
+  }
+  return duplicates.length;
 }
+getDuplicateCount(1, [1, 2, 3, 1, 4, 5, 6, 1, 7, 8, 9, 10, 11, 1, 12, 13]);
 
 /**
  * youGottaCalmDown(s):
@@ -55,6 +78,14 @@ function getDuplicateCount(x, numbers) {
  */
 function youGottaCalmDown(s) {
   // Your code here
+  for (let i = 0; i < s.length; i++)
+    if (s[i] == "!") {
+      let strs = [s];
+      s.slice(0, i);
+      console.log(`${strs}!`);
+    }
+  return s;
 }
+youGottaCalmDown("Hellooooo!!!!!");
 
 module.exports = { getOdds, getEvens, getDuplicateCount, youGottaCalmDown };
